8ee28598b13b524276d5f822105ce51c87a0b797
Vitor <vitaocodes@gmail.com>
2023-05-10 17:35:36 -0300

    properties-test, class Game, ORM, Game seed

---
+++ dslit/src/main/java/com/estudosspring/dslit/DslitApplication.java
@@ -0,0 +1,13 @@
+package com.estudosspring.dslit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslitApplication.class, args);
+	}
+
+}
---
+++ dslit/src/main/java/com/estudosspring/dslit/entities/Game.java
@@ -0,0 +1,39 @@
+package com.estudosspring.dslit.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+
+
+}
+
+
+
